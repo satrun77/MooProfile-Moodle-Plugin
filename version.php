6a1ac0a288b8f8db8ed9c420f40c815c1ec8ccf6
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2012-10-28 21:52:50 +1300

    update to be compatible with Moodle 2.3

--- version.php
+++ version.php
@@ -2,4 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2011091401;
+$plugin->version = 2011091402;
+$plugin->requires = 2011120500;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.2';
