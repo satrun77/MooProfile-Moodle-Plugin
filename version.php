9dfc80365c875fc51c61ca7cf306440a03345111
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2013-12-05 10:53:02 +1300

    increment the release version

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2013011902;
 $plugin->requires = 2012062500;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.1.3';
+$plugin->release = '1.1.4';
